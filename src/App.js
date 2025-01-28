cca92d053f04daeeb70c7755ea41724256ed40bf
alvera-v <alvera-v@student.42malaga.com>
2025-01-28 13:19:10 +0100

    Create react-dictionary app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <button type="button" class="btn btn-secondary btn-sm">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
