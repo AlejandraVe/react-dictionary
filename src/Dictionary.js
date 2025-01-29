5381b44e99e870ae08f6ba640d7fa4cdb763903d
alvera-v <alvera-v@student.42malaga.com>
2025-01-29 11:45:25 +0100

    Added axios and functions to get info from the API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [searchedWord, setSearchedWord] = useState("");
@@ -7,9 +8,15 @@ export default function Dictionary() {
     setSearchedWord(event.target.value);
   }
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${searchedWord}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
